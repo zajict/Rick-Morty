d53b41120b8a979c60a255fc246867878db5f511
Tijana Zajic <tijanazajic1204@gmail.com>
2023-02-25 16:13:18 +0100

    data class add

--- JS/data.js
+++ JS/data.js
@@ -1,1 +1,12 @@
-class (id, image, name, status, gender, origin, location)  
+class Character {
+    constructor(id, image, name, status, gender, origin, location, episode) {
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.status = status;
+        this.gender = gender;
+        this.origin = origin;
+        this.location = location;
+        this.episode = episode;
+    };
+};
